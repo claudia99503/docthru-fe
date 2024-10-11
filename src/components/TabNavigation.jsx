a8fdee1ab4d753693f02d6d1b1946025de3e1145
minseokim <claudia503@naver.com>
2024-10-11 14:59:31 +0900

    :sparkles: [Feat] TabNavigation 컴포넌트에 헤더 및 신규 챌린지 신청 버튼 추가, 버튼 클릭 시 신청 페이지로 라우팅 처리

--- src/components/TabNavigation.jsx
+++ src/components/TabNavigation.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useRouter } from 'next/router';
+import assets from '../variables/images';
 import styles from './TabNavigation.module.css';
 
-const TabNavigation = () => {
+const TabNavigation = ({ activeTab }) => {
   const router = useRouter();
 
   const handleTabClick = (path) => {
@@ -10,25 +11,35 @@ const TabNavigation = () => {
   };
 
   return (
-    <div className={styles.TabNavigation}>
-      <button
-        className={`${styles.tabButton} ${router.pathname === '/me' ? styles.active : ''}`}
-        onClick={() => handleTabClick('/me')}
-      >
-        참여중인 챌린지
-      </button>
-      <button
-        className={`${styles.tabButton} ${router.pathname === '/me/done' ? styles.active : ''}`}
-        onClick={() => handleTabClick('/me/done')}
-      >
-        완료한 챌린지
-      </button>
-      <button
-        className={`${styles.tabButton} ${router.pathname === '/me/application' ? styles.active : ''}`}
-        onClick={() => handleTabClick('/me/application')}
-      >
-        신청한 챌린지
-      </button>
+    <div>
+      <div className={styles.header}>
+        <h1 className={styles.title}>나의 챌린지</h1>
+        <button className={styles.newChallengeButton} onClick={() => router.push('/application')}>
+          <span>신규 챌린지 신청</span>
+          <img src={assets.icons.plusLarge} alt="plus Icon" className={styles.icon} />
+        </button>
+      </div>
+
+      <div className={styles.TabNavigation}>
+        <button
+          className={`${styles.tabButton} ${activeTab === 'ongoing' ? styles.active : ''}`}
+          onClick={() => handleTabClick('/me')}
+        >
+          참여 중인 챌린지
+        </button>
+        <button
+          className={`${styles.tabButton} ${activeTab === 'completed' ? styles.active : ''}`}
+          onClick={() => handleTabClick('/me/done')}
+        >
+          완료한 챌린지
+        </button>
+        <button
+          className={`${styles.tabButton} ${activeTab === 'applications' ? styles.active : ''}`}
+          onClick={() => handleTabClick('/me/application')}
+        >
+          신청한 챌린지
+        </button>
+      </div>
     </div>
   );
 };
