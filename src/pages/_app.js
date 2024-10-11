6125ef839c5e919d793f01e7a8306c9a3b713f43
amberim <im.amberim@gmail.com>
2024-10-11 16:25:36 +0900

    :art: [Style] reset.css 추가 및 이미지소스 추가

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css';
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
---
+++ src/variables/fonts.js
@@ -0,0 +1,6 @@
+import { Quantico } from "next/font/google";
+
+const quantico = Quantico({
+  weight: ["400", "700"],
+  subsets: ["korean"],
+});
