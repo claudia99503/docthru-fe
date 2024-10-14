97ad0b9b7fff165f14d44d02f609eabfea67d397
amberim <im.amberim@gmail.com>
2024-10-14 17:38:28 +0900

    :meno: [Docs] mockup 폴더 work.js 목업데이터 생성

--- src/pages/work/[id].jsx
+++ src/pages/work/[id].jsx
@@ -1,15 +1,23 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { work, feedback } from '@/variables/mockup';
 
 export default function WorkDetailPage() {
+  const router = useRouter();
+  const { id } = router.query;
+
   return (
     <>
       <Head>
         <title>작업물 상세페이지</title>
-        <meta name="description" content="작업물에 대한 상세 정보를 보여주는 페이지입니다." />
+        <meta
+          name="description"
+          content="작업물에 대한 상세 정보를 보여주는 페이지입니다."
+        />
       </Head>
-      <div>
-      <h1>작업물 상세페이지 내용</h1>
-      </div>
+      <section>
+        <h1>작업물 상세페이지 내용</h1>
+      </section>
     </>
   );
 }
