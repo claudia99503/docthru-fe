cd0cd8380accf4f6f1c60d90b5859c2bbde2a6fe
minseokim <claudia503@naver.com>
2024-10-14 15:49:50 +0900

    :sparkles: [Feat] ChallengeSearchbarLarge 컴포넌트 추가

---
+++ src/components/common/ChallengeSearchbarLarge.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './ChallengeSearchbarLarge.module.css';
+import images from '../../variables/images';
+
+const ChallengeSearchbar = ({ searchTerm, setSearchTerm }) => {
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value); // 검색어 업데이트
+  };
+
+  return (
+    <div className={styles.ChallengeSearchbarLarge}>
+      <img src={images.icons.search} alt="Search Icon" className={styles.icon} />
+      <input
+        type="text"
+        className={styles.input}
+        placeholder="챌린지 이름을 검색해보세요"
+        value={searchTerm}
+        onChange={handleInputChange}
+      />
+    </div>
+  );
+};
+
+export default ChallengeSearchbar;
+
