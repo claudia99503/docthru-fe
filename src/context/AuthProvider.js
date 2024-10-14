bb910c1d8117e89b1b06ba2c827f6a122368955b
amberim <im.amberim@gmail.com>
2024-10-14 15:12:46 +0900

    :sparkles: [Feat] AuthProvider 추가, _app 세팅, issue template 추가

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -2,94 +2,92 @@ import { useMutation, useQueryClient, useQuery } from '@tanstack/react-query';
 import { useRouter } from 'next/router';
 import { createContext, useContext, useEffect, useState } from 'react';
 
-const AuthContext = createContext({
-  user: null,
-  isLoading: false,
-  logIn: () => {},
-  logOut: () => {},
-  onModalOpen: () => {},
-});
+// const AuthContext = createContext({
+//   user: null,
+//   isLoading: false,
+//   logIn: () => {},
+//   logOut: () => {},
+//   onModalOpen: () => {},
+// });
 
-export function AuthProvider({ children }) {
-  const queryClient = useQueryClient();
-  const router = useRouter();
-  const { onModalOpen, GlobalModal: AuthModal } = useGlobalModal();
-  const [accessToken, setAccessToken] = useState(null);
+// export function AuthProvider({ children }) {
+//   const queryClient = useQueryClient();
+//   const [accessToken, setAccessToken] = useState(null);
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setAccessToken(localStorage.getItem('accessToken'));
-    }
-  }, []);
+//   useEffect(() => {
+//     if (typeof window !== 'undefined') {
+//       setAccessToken(localStorage.getItem('accessToken'));
+//     }
+//   }, []);
 
-  const { data, isLoading } = useQuery({
-    queryKey: ['user'],
-    queryFn: getUserMe,
-    staleTime: Infinity,
-    cacheTime: Infinity,
-    refetchOnWindowFocus: false,
-    refetchOnMount: false,
-    enabled: !!accessToken,
-  });
+//   const { data, isLoading } = useQuery({
+//     queryKey: ['user'],
+//     queryFn: getUserMe,
+//     staleTime: Infinity,
+//     cacheTime: Infinity,
+//     refetchOnWindowFocus: false,
+//     refetchOnMount: false,
+//     enabled: !!accessToken,
+//   });
 
-  const logInMutation = useMutation({
-    mutationFn: (data) => createLogIn(data),
-    onSuccess: (data) => {
-      const { accessToken } = data;
-      localStorage.setItem('accessToken', accessToken);
-      queryClient.invalidateQueries('user');
-    },
-  });
+//   const logInMutation = useMutation({
+//     mutationFn: (data) => createLogIn(data),
+//     onSuccess: (data) => {
+//       const { accessToken } = data;
+//       localStorage.setItem('accessToken', accessToken);
+//       queryClient.invalidateQueries('user');
+//     },
+//   });
 
-  const signUpMutation = useMutation({
-    mutationFn: (data) => createUser(data),
-    onSuccess: (data) => {
-      const { accessToken } = data;
+//   const signUpMutation = useMutation({
+//     mutationFn: (data) => createUser(data),
+//     onSuccess: (data) => {
+//       const { accessToken } = data;
 
-      localStorage.setItem('accessToken', accessToken);
-      queryClient.invalidateQueries('user');
-    },
-    onError: (error) => {
-      console.error(error.message);
-    },
-  });
+//       localStorage.setItem('accessToken', accessToken);
+//       queryClient.invalidateQueries('user');
+//     },
+//     onError: (error) => {
+//       console.error(error.message);
+//     },
+//   });
 
-  const logOut = () => {
-    localStorage.removeItem('accessToken');
-    queryClient.setQueriesData(['user'], null);
+//   const logOut = () => {
+//     localStorage.removeItem('accessToken');
+//     queryClient.setQueriesData(['user'], null);
 
-    console.log('로그아웃 됨');
-  };
+//     console.log('로그아웃 됨');
+//   };
 
-  return (
-    <AuthContext.Provider
-      value={{
-        user: data,
-        isLoading,
-        logIn: logInMutation,
-        signUp: signUpMutation,
-        logOut,
-        onModalOpen,
-      }}
-    >
-      {children}
-      <AuthModal />
-    </AuthContext.Provider>
-  );
-}
+//   return (
+//     <AuthContext.Provider
+//       value={{
+//         user: data,
+//         isLoading,
+//         logIn: logInMutation,
+//         signUp: signUpMutation,
+//         logOut,
+//         onModalOpen,
+//       }}
+//     >
+//       {children}
+//       <AuthModal />
+//     </AuthContext.Provider>
+//   );
+// }
 
-export function useAuth(required) {
-  const context = useContext(AuthContext);
-  const router = useRouter();
+// export function useAuth(required) {
+//   const context = useContext(AuthContext);
+//   const router = useRouter();
 
-  if (!context) {
-    throw new Error('Error: not used within AuthProvider');
-  }
+//   if (!context) {
+//     throw new Error('Error: not used within AuthProvider');
+//   }
 
-  useEffect(() => {
-    if (required && !context.user && !context.isLoading) {
-      router.push('/auth/login'), console.log('로그인 필요함');
-    }
-  }, [context.user, context.isLoading, router, required]);
-  return context;
-}
+//   useEffect(() => {
+//     if (required && !context.user && !context.isLoading) {
+//       router.push('/auth/login'), console.log('로그인 필요함');
+//     }
+//   }, [context.user, context.isLoading, router, required]);
+//   return context;
+// }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,52 @@
+import {
+  HydrationBoundary,
+  MutationCache,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+// import { AuthProvider } from '@/context/AuthProvider';
 import '@/styles/globals.css';
+import React from 'react';
 import Layout from '@/components/layouts/Layout';
 import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+            retry: 1,
+          },
+          mutation: {
+            retry: 1,
+          },
+        },
+        mutationCache: new MutationCache({
+          onError: (error) => {
+            console.error('Mutation Error', error.message || 'Unknown Error');
+          },
+        }),
+      })
+  );
+
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.svg" />
+        <link rel="icon" href="favicon.svg" />
       </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <QueryClientProvider client={queryClient}>
+        <HydrationBoundary state={pageProps.dehydratedState}>
+          {/* <AuthProvider> */}
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+          {/* </AuthProvider> */}
+        </HydrationBoundary>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   );
 }
