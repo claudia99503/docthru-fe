d10555b80b570097620235744df59a60918d8d3d
yoorli <lucca6042@naver.com>
2024-10-16 01:11:21 +0900

    :sparkles: [Feat] 홈화면 필터 드롭다운 틀 구현

---
+++ src/components/challenge/ChallengeDropdown.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import styles from './ChallengeDropdown.module.css';
+import images from '../../variables/images';
+
+const ChallengeDropdown = ({ onOptionChange }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState('필터');
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
+    onOptionChange(option); // 상위 컴포넌트로 선택된 옵션 전달
+    setIsOpen(false);
+  };
+
+  return (
+    <div className={styles.ChallengeDropdown}>
+      <button className={styles['dropdown-button']} onClick={toggleDropdown}>
+        <span className={styles['selected-text']}>필터</span>
+        <img
+          src={images.icons.filterBlack}
+          alt="Toggle Dropdown"
+          className={styles.icon}
+        />
+      </button>
+      {isOpen && (
+        <ul className={styles['dropdown-list']}>
+          <div className={styles['dropdown-out']}>
+            <span>필터</span>
+            <img src={images.icons.out} alt="out" onClick={toggleDropdown} style={{cursor:'pointer'}}/>
+          </div>
+          {[
+            'Next.js',
+            'Modern JS',
+            'API',
+            'Web',
+            'Career',
+            '공식 문서',
+            '블로그',
+            '진행중',
+            '마감',
+          ].map((option, index) => (
+            <li
+              key={index}
+              className={styles['dropdown-item']}
+              onClick={() => handleOptionClick(option)}
+            >
+              {option}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default ChallengeDropdown;
