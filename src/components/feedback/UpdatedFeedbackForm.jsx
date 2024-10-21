2aadf023a0f17a098405db39715ce7d0969dd9ae
amberim <im.amberim@gmail.com>
2024-10-21 13:46:27 +0900

    :recycle: [Refactor] Button cancel 스타일 추가

--- src/components/feedback/UpdatedFeedbackForm.jsx
+++ src/components/feedback/UpdatedFeedbackForm.jsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import styles from './UpdateFeedbackForm.module.css';
 import TextArea from '../common/form/TextArea';
 import { FEEDBACK } from '@/variables/formValidation';
+import Button from '../common/Button';
 
 export default function UpdateFeedbackForm({ onSubmit, initialData, onClick }) {
   const formMethods = useForm();
@@ -28,13 +29,27 @@ export default function UpdateFeedbackForm({ onSubmit, initialData, onClick }) {
         className={styles.UpdateFeedbackForm}
       >
         <TextArea name="content" validations={FEEDBACK.CONTENT} />
-        <div className={styles.btns}>
-          <button variant="cancel" onClick={onClick} className="small">
+        <div className={styles['button-container']}>
+          <Button
+            variant="cancel"
+            onClick={onClick}
+            className={styles.button}
+            borderRadius="8px"
+            fontSize="14px"
+            padding="4px 10px"
+          >
             취소
-          </button>
-          <button disabled={!isValid} type="submit" className="small">
+          </Button>
+          <Button
+            disabled={!isValid}
+            type="submit"
+            className={styles.button}
+            borderRadius="8px"
+            fontSize="14px"
+            padding="4px 10px"
+          >
             수정 완료
-          </button>
+          </Button>
         </div>
       </form>
     </FormProvider>
