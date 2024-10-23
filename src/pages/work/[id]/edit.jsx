6482efb610969c74abad4483c863f2cb9dbfe392
amberim <im.amberim@gmail.com>
2024-10-23 14:06:13 +0900

    :recycle: [Refactor] add /work/[id]/eidt 페이지

--- src/pages/work/[id]/edit.jsx
+++ src/pages/work/[id]/edit.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import Head from 'next/head';
 
-const EditPage = () => {
+export default function EditWorkPage() {
   return (
-    <div>
-      <h1>Edit Page</h1>
-      {/* 페이지 내용 */}
-    </div>
+    <>
+      <Head>
+        <title>작업물 수정페이지</title>
+        <meta
+          name="description"
+          content="작업물에 대한 상세 정보를 보여주는 수정하는페이지입니다."
+        />
+      </Head>
+      <h1>작업물 수정 페이지</h1>
+    </>
   );
-};
-
-export default EditPage;
+}
