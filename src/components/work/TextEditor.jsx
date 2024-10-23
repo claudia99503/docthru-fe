d45c93d4e15dbaa21c3aa12e9a30f9d827644ece
amberim <im.amberim@gmail.com>
2024-10-23 13:58:40 +0900

    :sparkles: [Feat] implement React-Quill

--- src/components/work/TextEditor.jsx
+++ src/components/work/TextEditor.jsx
@@ -1,39 +1,45 @@
-// import dynamic from 'next/dynamic';
-// import { useEffect, useRef, useState } from 'react';
-// import Loader from '../common/Loader';
-// import 'react-quill/dist/quill.snow.css';
+import dynamic from 'next/dynamic';
+import { useEffect, useRef, useState } from 'react';
+import Loader from '../common/Loader';
+import 'react-quill/dist/quill.snow.css';
+import EditorToolbar from './EditorToolBar';
 
-// const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
+const ReactQuill = dynamic(() => import('react-quill'), {
+  ssr: false,
+  loading: () => <p>Loading Editor...</p>,
+});
 
-// export default function TextEditor() {
-//   const [content, setContent] = useState('');
-//   const quillRef = useRef(null);
+export default function TextEditor() {
+  const [content, setContent] = useState('');
+  const quillRef = useRef(null);
 
-//   const modules = {
-//     toolbar: {
-//       container: [
-//         ['bold', 'italic', 'underline'],
-//         [{ align: [] }],
-//         [{ list: 'bullet' }, { list: 'ordered' }],
-//         [{ color: [] }],
-//       ],
-//     },
-//   };
+  const modules = {
+    toolbar: {
+      container: '#toolbar',
+      handlers: {},
+    },
+  };
 
-//   const handleContentChange = (value) => {
-//     setContent(value);
-//   };
+  const handleContentChange = (value) => {
+    setContent(value);
+  };
 
-//   return (
-//     <div>
-//       <ReactQuill
-//         ref={quillRef}
-//         value={content}
-//         onChange={handleContentChange}
-//         modules={modules}
-//         theme="snow"
-//       />
-//       <p>Editor content:{content}</p>
-//     </div>
-//   );
-// }
+  useEffect(() => {
+    if (quillRef.current) {
+      console.log('커스텀툴바');
+    }
+  }, [quillRef]);
+
+  return (
+    <div>
+      <EditorToolbar />
+      <ReactQuill
+        ref={quillRef}
+        value={content}
+        onChange={handleContentChange}
+        modules={modules}
+      />
+      <p>Editor content:{content}</p>
+    </div>
+  );
+}
