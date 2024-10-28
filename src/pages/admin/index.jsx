b2ae72ddf21ea45e58dd0cbc54c8f5e0dc4ad639
yoolri <lucca6042@gmail.com>
2024-10-28 11:34:13 +0900

    :sparkles: [Feat] admin - 목록 페이지 생성

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -1,6 +1,60 @@
-import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
-export default function AdminChallengePage() {
+import { useGetChallenges } from '@/service/queries/challenge';
+
+import Head from 'next/head';
+import ChallengeSearchBarLarge from '@/components/common/ChallengeSearchBarLarge';
+import images from '@/variables/images';
+import Loader from '@/components/common/Loader';
+import Pagination from '@/components/application/Pagination';
+
+import AllCardSection from '@/components/challenge/AllCardSection';
+import ChallengeDropdown from '@/components/challenge/ChallengeDropdown';
+
+import styles from '@/styles/pages/Home.module.css';
+
+// import { dehydrate, QueryClient } from '@tanstack/react-query';
+// import { getChallengeList } from '@/service/api/challenge';
+import { useMediaQuery } from 'react-responsive';
+
+export default function Home(initialData) {
+  const isMobile = useMediaQuery({ query: '(max-width: 743px)' });
+  const router = useRouter();
+  
+  const [limit, setLimit] = useState(5);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedOption, setSelectedOption] = useState({});
+
+  const { data = initialData, isPending } = useGetChallenges(selectedOption, {
+    enabled: true,
+  });
+  const { meta = {}, list = [] } = data || {};
+  const { totalPages } = meta;
+
+  const handleOptionChange = (option) => {
+    setSelectedOption((pev) => ({...pev, ...option }));
+  };
+
+  useEffect(() => {
+    setLimit(isMobile ? 4 : 5);
+  }, [isMobile]);
+
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [searchTerm, limit, selectedOption?.field, selectedOption?.docType, selectedOption?.progress]);
+
+  useEffect(() => {
+    const option = {
+      keyword: searchTerm,
+      page: currentPage,
+      limit: limit
+    }
+    
+    handleOptionChange(option)
+  }, [currentPage, limit, searchTerm, selectedOption?.field, selectedOption?.docType, selectedOption?.progress]);
+
   return (
     <>
       <Head>
@@ -10,9 +64,71 @@ export default function AdminChallengePage() {
           content="관리자가 챌린지를 보는 페이지입니다."
         />
       </Head>
-      <div>
-        <h1>챌린지 보기 내용</h1>
+
+      <div className={styles.title}>
+        <span>챌린지 목록</span>
+        <button
+          className={styles['new-challenge-button']}
+          onClick={() => router.push('/application')}
+        >
+          <span>신규 챌린지 신청</span>
+          <img
+            src={images.icons.plus}
+            alt="plus Icon"
+            className={styles.icon}
+          />
+        </button>
       </div>
+      <div className={styles.SearchContainer}>
+        <ChallengeDropdown onOptionChange={setSelectedOption} />
+        <ChallengeSearchBarLarge
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+        />
+      </div>
+      {isPending ? (
+        <Loader />
+      ) : (
+        <>
+          <div className={styles['card-container']}>
+            <AllCardSection
+              list={list}
+              site={'home'}
+              isAdmin={true}
+            />
+          </div>
+          {list.length > 0 && (
+            <Pagination
+              currentPage={currentPage}
+              totalPages={totalPages} // 계산된 totalPages 사용
+              onPageChange={setCurrentPage}
+            />
+          )}
+        </>
+      )}
     </>
   );
 }
+
+// 서버 사이드 렌더링에서 데이터 가져오기
+// export async function getServerSideProps(context) {
+//   const queryClient = new QueryClient();
+
+//   const initialOptions = {
+//     field: '',
+//     docType: '',
+//     status: '',
+//     page: 1,
+//     limit: 5,
+//   };
+
+//   await queryClient.prefetchQuery(['challenges', initialOptions], () =>
+//     getChallengeList(initialOptions)
+//   );
+
+//   return {
+//     props: {
+//       dehydratedState: dehydrate(queryClient),
+//     },
+//   };
+// }
