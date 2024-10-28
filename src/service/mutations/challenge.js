3aba087de32178fa17c5539a4a56ab07dc1289bc
yoolri <lucca6042@gmail.com>
2024-10-28 10:56:24 +0900

    :sparkle: [Feat] 도전하기 버튼 로직 변경
    도전하기 버튼 클릭 시 챌린지 참여 상태로 변경

--- src/service/mutations/challenge.js
+++ src/service/mutations/challenge.js
@@ -1,14 +1,21 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { deleteChallengeParticipation } from '../api/challenge';
+import { createChallenge, deleteChallengeParticipation } from '../api/challenge';
 import { challengeKey } from '@/variables/queryKeys';
 
-export function useGiveUpChallenge(id) {
+export function useMutateChallenge(id) {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: (id) => deleteChallengeParticipation(id),
+    mutationFn: (data) => {
+      const { id, isParticipate } = data;
+      if (isParticipate) {
+        return createChallenge(id);
+      } else {
+        return deleteChallengeParticipation(id);
+      }
+    },
     onSuccess: () => {
-      console.log('successMutation: 챌린지를 포기하였습니다');
+      console.log('successMutation');
       queryClient.invalidateQueries({
         queryKey: challengeKey.details(id),
       });
