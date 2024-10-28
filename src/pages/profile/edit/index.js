c28525944b0ec300c995a2bcd9d064f9ed6fd3f1
minsu <a2238737@gmail.com>
2024-10-28 18:52:27 +0900

    :sparkles: [Feat] : 내정보 수정 추가 및 router경로 수정

--- src/pages/profile/edit/index.js
+++ src/pages/profile/edit/index.js
@@ -1,7 +1,77 @@
 import React from 'react';
+import styles from './EditProfilePage.module.css';
+import { useState } from 'react';
+import images from '../../../variables/images';
+import Image from 'next/image';
 
 function EditProfilePage() {
-  return <div>프로필 수정 페이지</div>;
+  const [profileImg, setProfileImg] = useState(null);
+  const [profileName, setProfileName] = useState('');
+
+  const handleProfileNameChange = (event) => {
+    setProfileName(event.target.value);
+  };
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const base64URL = reader.result;
+        setProfileImg(base64URL);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const data = {
+    profileImg,
+    profileName,
+  };
+
+  return (
+    <div className={styles.container}>
+      <div style={{ position: 'relative' }}>
+        <img
+          className={styles['profileImg']}
+          src={profileImg || images.images.profile}
+        />
+
+        <button
+          className={styles.gearButton}
+          onClick={() => document.getElementById('fileInput').click()}
+        >
+          <img
+            style={{ width: '15px' }}
+            src={images.icons.gear}
+            alt="Settings"
+          />
+        </button>
+
+        <input
+          id="fileInput"
+          type="file"
+          style={{ display: 'none' }}
+          onChange={(e) => handleFileUpload(e)}
+        />
+      </div>
+
+      <div className={styles['profileInfo']}>
+        <input
+          type="text"
+          placeholder="Name"
+          className={styles['profileInput']}
+          value={profileName}
+          onChange={handleProfileNameChange}
+        />
+        <button
+          onClick={() => console.log(data)}
+          className={styles['profileButton']}
+        >
+          수정
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default EditProfilePage;
